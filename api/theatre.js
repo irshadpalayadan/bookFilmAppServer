9c5a043923da8474b2e771a51bc6dfc1bf5239f1
Irshad Arippayil <iarippay@oradev.oraclecorp.com>
2018-11-27 20:52:20 +0530

    heroku add test

--- api/theatre.js
+++ api/theatre.js
@@ -3,7 +3,7 @@ var theatre = require('express').Router();
 
 theatre.get('/', (req, res) => {
 
-    res.status(200).json({ message : 'theatre check success'});
+    res.status(200).json({ message : 'theatre check success done by irshad'});
 });
 
 module.exports = theatre;
